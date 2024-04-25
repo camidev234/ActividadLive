a5d25b25905e601c791c1ddea804f302db94134c
camidev234 <camilo222andresace@gmail.com>
2024-04-24 19:26:31 -0500

    tres

--- resources/views/livewire/show-tickets.blade.php
+++ resources/views/livewire/show-tickets.blade.php
@@ -3,11 +3,9 @@
 
     <div class="dos lg:w-[50%] md:w-[100%] sm:w-[100%] flex flex-col items-center pt-9 h-[100vh] overflow-auto">
         <h1 class="font-bold text-2xl mb-6">Listado de boletas</h1>
-        @if(!$tickets->isEmpty())
         <div class="search mb-5 lg:w-[50%] sm:w-[30%] ">
             <input type="text" placeholder="Buscar boletas por numero" class="lg:w-full pl-2 pt-1 pr-2 pb-2 rounded-md border-[1px] border-zinc-900 outline-none" wire:model.live="search_word">
         </div>
-        @endif
         @forelse($tickets as $ticket)
         <div class="card lg:w-[50%] sm:w-[60%] bg-zinc-900 p-3 border-b-[7px] rounded-md border-b-zinc-900 mb-4 shadow-lg">
             <div class="w-full lg:flex lg:items-center sm:flex sm:justify-around lg:justify-between">
